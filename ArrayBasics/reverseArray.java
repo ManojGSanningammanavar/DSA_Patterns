1dd97e2f31e0bf828e4e6d0d18589a76adaaa6a7
ManojGSanningammanavar <manojsanningmmanavar@gmail.com>
2026-06-25 15:37:18 +0530

    REVERSE ARRAY

--- ArrayBasics/reverseArray.java
+++ ArrayBasics/reverseArray.java
@@ -4,9 +4,12 @@ import java.util.*;
 public class reverseArray {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+
     System.out.println("Enter the array size: ");
     int n = sc.nextInt();
+    
     int arr[] = new int[n];
+
     System.out.println("Enter the array elements : ");
     for(int i =0; i<n; i++){
         arr[i] = sc.nextInt();
